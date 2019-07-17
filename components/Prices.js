e80bc9b98a701661568cf51dd8e947cc3c2150cc
Christina <christina.hastenrath@getpostman.com>
2019-07-17 11:14:02 -0700

    added in option in Prices to update prices state and change currency output

--- components/Prices.js
+++ components/Prices.js
@@ -6,13 +6,35 @@ class Prices extends React.Component {
     }
 
     render() {
+
+        // cannot map through bpi data because it is returned as an object and not as an array
+        let list = '' ;
+
+        if(this.state.currency === 'USD') {
+            list = <li className="list-group-item">
+            Bitcoin rate for {this.props.bpi[this.state.currency].description} : <span className="badge badge-primary">{this.props.bpi[this.state.currency].code}</span> <strong>{this.props.bpi[this.state.currency].rate}</strong>
+            </li>
+        } else if (this.state.currency === 'GBP') {
+            list = <li className="list-group-item">
+            Bitcoin rate for {this.props.bpi[this.state.currency].description} : <span className="badge badge-primary">{this.props.bpi[this.state.currency].code}</span> <strong>{this.props.bpi[this.state.currency].rate}</strong>
+            </li>
+        } else if (this.state.currency === 'EUR') {
+            list = <li className="list-group-item">
+            Bitcoin rate for {this.props.bpi[this.state.currency].description} : <span className="badge badge-primary">{this.props.bpi[this.state.currency].code}</span> <strong>{this.props.bpi[this.state.currency].rate}</strong>
+            </li>
+        }
+
         return (
             <div>
                 <ul className="list-group">
-                    <li className="list-group-item">
-                        Bitcoin rate for {this.props.bpi[this.state.currency].description} : <span className="badge badge-primary">{this.props.bpi[this.state.currency].code}</span> <strong>{this.props.bpi[this.state.currency].rate}</strong>
-                    </li>
+                    {list}
                 </ul>
+                <br/>
+                <select onChange={ e => this.setState({currency: e.target.value})} className="form-control">
+                    <option value="USD">USD</option>
+                    <option value="GBP">GBP</option>
+                    <option value="EUR">EUR</option>
+                </select>
             </div>
         )
     }
