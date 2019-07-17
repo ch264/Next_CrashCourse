53d63e6568810dba423b6882ad205fe4ce9606f8
Christina <christina.hastenrath@getpostman.com>
2019-07-17 10:27:11 -0700

    added links to navigate but have to include them in every page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,13 @@
+
+import Link from 'next/link'
+
 const about = () => (
     <div>
+        <ul>
+            <li><Link href="/"><a>Home</a></Link></li>
+            <li><Link href="/about"><a>About</a></Link></li>
+        </ul>
+        
         <h1>About BitPrice</h1>
         <p>App to view Bit Prices</p>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+
+
 const index = () => (
     <div>
+        <ul>
+            <li><Link href="/"><a>Home</a></Link></li>
+            <li><Link href="/about"><a>About</a></Link></li>
+        </ul>
         <h1>Hello Next</h1>
     </div>
 );
