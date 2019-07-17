1eaca380002631d60873a19551dad67c162fe4a2
Christina <christina.hastenrath@getpostman.com>
2019-07-17 11:07:45 -0700

    create prices component to pull in data from fetch from index.js

--- components/Prices.js
+++ components/Prices.js
@@ -8,7 +8,11 @@ class Prices extends React.Component {
     render() {
         return (
             <div>
-                Prices
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        Bitcoin rate for {this.props.bpi[this.state.currency].description} : <span className="badge badge-primary">{this.props.bpi[this.state.currency].code}</span> <strong>{this.props.bpi[this.state.currency].rate}</strong>
+                    </li>
+                </ul>
             </div>
         )
     }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Layout from '../components/Layout';
 import Fetch from 'isomorphic-unfetch';
 
-import Prices from '../components/Prices'
+import Prices from '../components/Prices';
 
-const index = (props) => (
+const Index = (props) => (
     <Layout>
         <div>
             <h1>Welcome to BitPrice</h1>
@@ -14,18 +14,18 @@ const index = (props) => (
     </Layout>
 );
 
-index.getInitialProps = async function() {
+Index.getInitialProps = async function() {
     const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
     const data = await res.json();
 
     // put bpi object from fetch call into bpi prop
     return {
-        bpi: data
-    }
+        bpi: data.bpi
+    };
 }
 
 // keep styles component based
 
 
 
-export default index;
+export default Index;
